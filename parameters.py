4e67ac3e4f6f5b67e3817356d673c0d497df9452
Oscar Garcia-Montero <garcia@Oscars-MacBook-Air-7.local>
2026-03-21 11:03:37 +0100

    test

--- parameters.py
+++ parameters.py
@@ -7,13 +7,16 @@ import numpy as np
 # pT bins: logarithmically spaced from 0.05 to 3.0 GeV.
 # Log spacing gives finer resolution at low pT and coarser bins at high pT 
 # We start at 0.05 GeV since particles below ~50 MeV/c are below the iSS sampling threshold anyway.
-PT_MIN   = 0.1                            # lower edge [GeV]
-PT_MAX   = 5.0                               # upper edge [GeV]
-N_PT     = 25                                # number of bins
-PT_BINS  = np.logspace(np.log10(PT_MIN),
-                       np.log10(PT_MAX),
-                       N_PT + 1)             # shape (61,) — 60 bin edges
-PT_CENTS = np.sqrt(PT_BINS[:-1] * PT_BINS[1:])  # geometric centre of each bin
+                              # upper edge [GeV]
+
+PT_BINS  = np.array([0.1,0.5,0.75,1.0,1.4,1.8,2.2,2.4,2.6,3.0,4.0,5.0])
+N_PT     = len(PT_BINS)
+PT_MIN   = PT_BINS[0]                           # lower edge [GeV]
+PT_MAX   = PT_BINS[-1]
+# np.logspace(np.log10(PT_MIN),
+#                        np.log10(PT_MAX),
+#                        N_PT + 1)             # shape (61,) — 60 bin edges
+PT_CENTS = (PT_BINS[:-1] + PT_BINS[1:])/2  # geometric centre of each bin
 
 # ── coarse pT grid: for flow vn(pT) ───────────────────────────────────
 # Fewer bins means more particles per bin, more stable Q-vectors.
@@ -25,7 +28,7 @@ PT_MAX_FLOW  = PT_BINS_FLOW[-1]
 N_PT_FLOW    = len(PT_BINS_FLOW)
 # PT_BINS_FLOW = np.logspace(np.log10(PT_MIN_FLOW), np.log10(PT_MAX_FLOW),
 #                            N_PT_FLOW + 1)
-PT_CENTS_FLOW = np.sqrt(PT_BINS_FLOW[:-1] * PT_BINS_FLOW[1:])
+PT_CENTS_FLOW = np.sqrt(PT_BINS_FLOW[:-1] + PT_BINS_FLOW[1:])/2
 
 ALICE_TRACK_SEL = [PT_MIN,50.]
 
